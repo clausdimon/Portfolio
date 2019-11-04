0dcb000945f6d40ac91b852b76d32f35275f5d0d
Claus Nordahl Dimon <53813074+clausdimon@users.noreply.github.com>
2019-11-04 12:19:54 +0100

    homework
    little things insert while home sick

--- first_prokekt.php
+++ first_prokekt.php
@@ -4,7 +4,7 @@
     if ($conn->connect_error) {
         die("Connection failed;" . $conn->connect_error);
     }
-    $sql = "SELECT*FROM portfolio_projekter ORDER BY ID ASC LIMIT 1";
+    $sql = "SELECT*FROM portfolio_projekter ORDER BY ID DESC LIMIT 1";
     $result = $conn->query($sql);
     $list = array();
     while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
---
+++ projekter.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="Stylesheet.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <Header>
+        <h1>Portfolio</h1>
+    </Header>
+    <div class="topnav">
+        <a href="#" class="active">Home</a>
+        <a href="#">Projekter</a>
+        <a href="#">About Me</a>
+    </div>
+    <br>
+    <div id="projekter">
+    </div>
+    <footer>
+        <div class="row">
+            <div class="colum">
+                <p><b>Made by</b>: Claus Dimon</p>
+            </div>
+            <div class="colum">
+                <p><b>Contact Phonenr</b>: <a href="tel:+4581754402">+4581754402</a></p>
+            </div>
+            <div class="colum">
+                <p><b>Contact email</b>: <a href="mailto:Claus.n.dimon@live.dk">Claus.n.dimon@live.dk</a></p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
